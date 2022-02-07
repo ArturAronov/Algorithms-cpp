b3524702ce5e4d7839acd84ed4d1e2adc47bce9a
Artur Aronov <unionco@icloud.com>
2022-02-08 01:25:59 +0800

    added new algorithm

---
+++ leetCode | 389. Find the Difference/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+  char findTheDifference(string s, string t) {
+    int result=0;
+    
+    for(int i=0; i<s.size(); i++){
+      result+=t[i];
+      result-=s[i];
+    }
+    
+    result+=t[t.size()-1];
+    
+    return result;
+  }
+};
